3d7fc8c71967de49a546c8263f797aec88f1aa5c
workcha <1094461298@qq.com>
2022-03-30 15:55:43 +0800

    添加RemoveDuplicateElement函数

--- common.go
+++ common.go
@@ -7,3 +7,16 @@ func GetDomainUrl(url string) string {
 	u := strings.Split(url, "/")
 	return u[0] + "://" + u[2] + "/"
 }
+
+// 字符串数组去重
+func RemoveDuplicateElement(languages []string) []string {
+	result := make([]string, 0, len(languages))
+	temp := map[string]struct{}{}
+	for _, item := range languages {
+		if _, ok := temp[item]; !ok {
+			temp[item] = struct{}{}
+			result = append(result, item)
+		}
+	}
+	return result
+}
