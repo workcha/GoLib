673d02e5d17c3df7ff51631ef12ad9faa1ee7cba
workcha <1094461298@qq.com>
2022-04-01 22:32:25 +0800

    自己重写xhttp，更加符合需求

--- xhttp.go
+++ xhttp.go
@@ -1,129 +1,141 @@
 package GoLib
 
 import (
-	"context"
-	"crypto/md5"
-	"encoding/hex"
-	"github.com/xiecat/xhttp"
-	"golang.org/x/net/html"
+	"bytes"
+	"fmt"
 	"golang.org/x/net/publicsuffix"
+	"golang.org/x/text/encoding/traditionalchinese"
+	"golang.org/x/text/transform"
+	"io"
+	"mime/multipart"
 	"net/http"
 	"net/http/cookiejar"
-	"regexp"
+	"net/textproto"
+	"net/url"
 	"strings"
+	"time"
 )
 
-var client *xhttp.Client = nil
+type Http struct {
+	Proxy   string
+	Header  map[string]string
+	TimeOut int
+	Client  *http.Client
+}
 
-func getClient(headers map[string]string) *xhttp.Client {
-	//如果已经初始化则不再进行初始化
-	if client != nil && headers == nil {
-		return client
+//正常请求
+func (h *Http) httpRequest(method, url, body, tp string) *http.Response {
+	if h.Client == nil {
+		h.Init()
 	}
-	options := xhttp.DefaultClientOptions()
-	//设置headers
-	HEADER := map[string]string{
-		//"accept": "text/javascript, application/javascript, application/ecmascript, application/x-ecmascript, */*; q=0.01",
-		//"accept-encoding": "gzip, deflate, br",
-		"accept-language": "zh-CN,zh;q=0.9",
-		"Referer":         "https://item-paimai.taobao.com/pmp_item/609160317276.htm?s=pmp_detail&spm=a213x.7340941.2001.61.1aec2cb6RKlKoy",
-		"sec-fetch-mode":  "cors",
-		"sec-fetch-site":  "same-origin",
-		"user-agent":      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36",
-		//"x-requested-with": "XMLHttpRequest",
+	I := bytes.NewReader([]byte(body))
+	O := transform.NewReader(I, traditionalchinese.Big5.NewEncoder())
+	requests, _ := http.NewRequest(method, url, O)
+	for k, v := range h.Header {
+		requests.Header.Add(k, v)
 	}
-	if headers != nil {
-		for k := range headers {
-			HEADER[k] = headers[k]
-		}
+	//json格式传输
+	if tp == "json" {
+		requests.Header.Add("Content-Type", "application/json")
+	} else if tp == "xml" {
+		//xml格式传输
+		requests.Header.Add("Content-Type", "text/xml")
+	} else {
+		//普通格式传输
+		requests.Header.Add("Content-Type", "application/x- www-form-urlencoded")
 	}
+	response, _ := h.Client.Do(requests)
+	return response
+}
 
-	options.Headers = HEADER
-	options.DialTimeout = 6
-	options.MaxRespBodySize = 2 << 60
-	//options.Proxy = "http://127.0.0.1:8080"
-	// 如果要继承cookie，传入cookie jar；否则填nil。
+//JSON请求
+func (h *Http) JsonRequest(url, body string) *http.Response {
+	return h.httpRequest("POST", url, body, "json")
+}
+
+//xml请求
+func (h *Http) XMLRequest(url, body string) *http.Response {
+	return h.httpRequest("POST", url, body, "xml")
+}
+
+//GET请求
+func (h *Http) GET(url string) {
+	h.httpRequest("GET", url, "", "")
+}
+
+//POST请求
+func (h *Http) POST(url string, body string) {
+	h.httpRequest("POST", url, body, "")
+}
+
+//初始化client
+func (h *Http) Init() {
 	cookieJar, _ := cookiejar.New(&cookiejar.Options{PublicSuffixList: publicsuffix.List})
-
-	// 创建client
-	client, _ = xhttp.NewClient(options, cookieJar)
-	return client
-}
-
-func REQUEST(url, body, method string, headers map[string]string) *xhttp.Response {
-	if method == "GET" {
-		return GET(url, headers)
-	} else if method == "POST" {
-		return POST(url, body, headers)
+	if h.Proxy != "" {
+		proxy, _ := url.Parse(h.Proxy)
+		h.Client = &http.Client{Jar: cookieJar, Timeout: time.Duration(h.TimeOut) * time.Second, Transport: &http.Transport{Proxy: http.ProxyURL(proxy)}}
+	} else {
+		h.Client = &http.Client{Jar: cookieJar, Timeout: time.Duration(h.TimeOut) * time.Second}
 	}
-	return nil
 }
 
-//发生GET数据包
-func GET(url string, headers map[string]string) *xhttp.Response {
-	c := getClient(headers)
-	// 构造请求
-	hr, _ := http.NewRequest("GET", url, nil)
-	req := &xhttp.Request{RawRequest: hr}
-	// 发起请求
-	ctx := context.Background()
-	resp, _ := c.Do(ctx, req)
-	return resp
+//CreateFormFile附属函数
+var quoteEscaper = strings.NewReplacer("\\", "\\\\", `"`, "\\\"")
+
+func escapeQuotes(s string) string {
+	return quoteEscaper.Replace(s)
 }
 
-//发生POST数据包
-func POST(url, body string, headers map[string]string) *xhttp.Response {
-	c := getClient(headers)
-	// 构造请求
-	hr, _ := http.NewRequest("POST", url, strings.NewReader(body))
-	req := &xhttp.Request{RawRequest: hr}
-	// 发起请求
-	ctx := context.Background()
-	resp, _ := c.Do(ctx, req)
-	return resp
+//重写multipart.Writer.CreateFormFile()函数实现可以控制Content-Type内容
+func CreateFormFile(fieldname, filename, contentType string, w *multipart.Writer) (io.Writer, error) {
+	h := make(textproto.MIMEHeader)
+	h.Set("Content-Disposition",
+		fmt.Sprintf(`form-data; name="%s"; filename="%s"`,
+			escapeQuotes(fieldname), escapeQuotes(filename)))
+	h.Set("Content-Type", contentType)
+	return w.CreatePart(h)
 }
 
-//获取文件md5
-func GetMd5(response *xhttp.Response) string {
-	if response.GetStatus() == 200 {
-		h := md5.New()
-		h.Write(response.Body)
-		return hex.EncodeToString(h.Sum(nil))
-	}
-	return " "
-}
-
-func trimTitleTags(title string) string {
-	// trim <title>*</title>
-	titleBegin := strings.Index(title, ">")
-	titleEnd := strings.Index(title, "</")
-	return title[titleBegin+1 : titleEnd]
-}
-
-//获取title
-func GetTitle(r *xhttp.Response) (title string) {
-	if r.GetStatus() == 200 {
-		body := string(r.GetBody())
-		var re = regexp.MustCompile(`(?im)<\s*title.*>(.*?)<\s*/\s*title>`)
-		for _, match := range re.FindAllString(body, -1) {
-			title = html.UnescapeString(trimTitleTags(match))
-			break
-		}
-		// Non UTF-8
-		if contentTypes, ok := r.GetHeaders()["Content-Type"]; ok {
-			contentType := strings.ToLower(strings.Join(contentTypes, ";"))
-			// special cases
-			if strings.Contains(contentType, "charset=gb2312") || strings.Contains(contentType, "charset=gbk") {
-				titleUtf8, err := Decodegbk([]byte(title))
-				if err != nil {
-					return
-				}
-
-				return string(titleUtf8)
+//文件上传
+func (h *Http) FileUpload(fieldName, fileName, url, contentType string, fileContent []byte, params map[string]string) *http.Response {
+	/*
+		根据文件上传的底层代码重写
+		req, err := NewRequest("POST", url, body)
+			if err != nil {
+				return nil, err
 			}
-		}
-		return
+			req.Header.Set("Content-Type", contentType)
+			return c.Do(req)
+	*/
+	if h.Client == nil {
+		h.Init()
 	}
-	return ""
 
+	body := new(bytes.Buffer)
+	mulWriter := multipart.NewWriter(body)
+	//添加参数
+	for k, v := range params {
+		err := mulWriter.WriteField(k, v)
+		if err != nil {
+			return nil
+		}
+	}
+	file, _ := CreateFormFile(fieldName, fileName, contentType, mulWriter)
+	//file, err := mulWriter.CreateFormFile(fieldName, fileName)
+	_, err := file.Write(fileContent)
+	if err != nil {
+		return nil
+	}
+	contentType2 := mulWriter.FormDataContentType()
+	err = mulWriter.Close()
+	if err != nil {
+		return nil
+	}
+	requests, _ := http.NewRequest("POST", url, body)
+	for k, v := range h.Header {
+		requests.Header.Add(k, v)
+	}
+	requests.Header.Add("Content-Type", contentType2)
+	response, _ := h.Client.Do(requests)
+	return response
 }
--- xhttp_test.go
+++ xhttp_test.go
@@ -1,30 +1,112 @@
 package GoLib
 
-import (
-	"testing"
-)
+import "testing"
 
-func TestGET(t *testing.T) {
-	resp := GET("https:://oa.dgut.edu.cn/", nil)
-	println(resp.GetStatus())
-	println(string(resp.GetBody()))
-}
+func TestGetClient(t *testing.T) {
+	header := map[string]string{
+		//"accept": "text/javascript, application/javascript, application/ecmascript, application/x-ecmascript, */*; q=0.01",
+		//"accept-encoding": "gzip, deflate, br",
+		"accept-language": "zh-CN,zh;q=0.9",
+		"Referer":         "https://item-paimai.taobao.com/pmp_item/609160317276.htm?s=pmp_detail&spm=a213x.7340941.2001.61.1aec2cb6RKlKoy",
+		"sec-fetch-mode":  "cors",
+		"sec-fetch-site":  "same-origin",
+		"user-agent":      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36",
+		//"x-requested-with": "XMLHttpRequest",
+	}
+	http := Http{Proxy: "http://127.0.0.1:8080", Header: header, TimeOut: 3}
 
-func TestPOST(t *testing.T) {
-	resp := POST("https://www.baidu.com", "123456", nil)
-	println(string(resp.GetBody()))
-}
+	/*
+		GET /8899.php HTTP/1.1
+		Host: 127.0.0.1
+		User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36
+		Accept-Language: zh-CN,zh;q=0.9
+		Content-Type: application/x- www-form-urlencoded
+		Referer: https://item-paimai.taobao.com/pmp_item/609160317276.htm?s=pmp_detail&spm=a213x.7340941.2001.61.1aec2cb6RKlKoy
+		Sec-Fetch-Mode: cors
+		Sec-Fetch-Site: same-origin
+		Connection: close
 
-func TestREQUEST(t *testing.T) {
+	*/
+	http.GET("http://127.0.0.1/8899.php")
 
-}
+	/*
+		POST /8899.php HTTP/1.1
+		Host: 127.0.0.1
+		User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36
+		Accept-Language: zh-CN,zh;q=0.9
+		Content-Type: application/x- www-form-urlencoded
+		Referer: https://item-paimai.taobao.com/pmp_item/609160317276.htm?s=pmp_detail&spm=a213x.7340941.2001.61.1aec2cb6RKlKoy
+		Sec-Fetch-Mode: cors
+		Sec-Fetch-Site: same-origin
+		Content-Length: 9
+		Connection: close
 
-func TestGetMd5(t *testing.T) {
-	resp := GET("https://www.baidu.com/favicon.ico", nil)
-	print(GetMd5(resp))
-}
+		A=1&N2=12
+	*/
+	http.POST("http://127.0.0.1/8899.php", "A=1&N2=12")
 
-func TestGetTitle(t *testing.T) {
-	resp := GET("https://www.baidu.com/", nil)
-	print(GetTitle(resp))
+	/*
+		POST /8899.php HTTP/1.1
+		Host: 127.0.0.1
+		User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36
+		Accept-Language: zh-CN,zh;q=0.9
+		Content-Type: application/json
+		Referer: https://item-paimai.taobao.com/pmp_item/609160317276.htm?s=pmp_detail&spm=a213x.7340941.2001.61.1aec2cb6RKlKoy
+		Sec-Fetch-Mode: cors
+		Sec-Fetch-Site: same-origin
+		Content-Length: 9
+		Connection: close
+
+		{"a":"1"}
+	*/
+	http.JsonRequest("http://127.0.0.1/8899.php", "{\"a\":\"1\"}")
+
+	/*
+		POST /8899.php HTTP/1.1
+		Host: 127.0.0.1
+		User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36
+		Accept-Language: zh-CN,zh;q=0.9
+		Content-Type: text/xml
+		Referer: https://item-paimai.taobao.com/pmp_item/609160317276.htm?s=pmp_detail&spm=a213x.7340941.2001.61.1aec2cb6RKlKoy
+		Sec-Fetch-Mode: cors
+		Sec-Fetch-Site: same-origin
+		Content-Length: 159
+		Connection: close
+
+		<?xml version="1.0" encoding="ISO-8859-1"?>
+		<note>
+		<to>George</to>
+		<from>John</from>
+		<heading>Reminder</heading>
+		<body>Don't forget the meeting!</body>
+		</note>
+	*/
+	http.XMLRequest("http://127.0.0.1/8899.php", "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>\n<note>\n<to>George</to>\n<from>John</from>\n<heading>Reminder</heading>\n<body>Don't forget the meeting!</body>\n</note>")
+
+	/*
+		POST /8899.php HTTP/1.1
+		Host: 127.0.0.1
+		User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36
+		Content-Length: 349
+		Accept-Language: zh-CN,zh;q=0.9
+		Content-Type: multipart/form-data; boundary=67c9c129a6b68b717510fdff951a65ac97a9134f383bf679eb91512d77de
+		Referer: https://item-paimai.taobao.com/pmp_item/609160317276.htm?s=pmp_detail&spm=a213x.7340941.2001.61.1aec2cb6RKlKoy
+		Sec-Fetch-Mode: cors
+		Sec-Fetch-Site: same-origin
+		Connection: close
+
+		--67c9c129a6b68b717510fdff951a65ac97a9134f383bf679eb91512d77de
+		Content-Disposition: form-data; name="param"
+
+		1
+		--67c9c129a6b68b717510fdff951a65ac97a9134f383bf679eb91512d77de
+		Content-Disposition: form-data; name="file"; filename="1.png"
+		Content-Type: image/png
+
+		I'am content
+		--67c9c129a6b68b717510fdff951a65ac97a9134f383bf679eb91512d77de--
+
+	*/
+	//文件上传
+	http.FileUpload("file", "1.png", "http://127.0.0.1/8899.php", "image/png", []byte("I'am content"), map[string]string{"param": "1"})
 }
